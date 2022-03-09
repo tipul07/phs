5bffbba8c4dc5f7aa69542212af831ba3b7e811b
Andrei Orghici <aorghici@gmail.com>
2022-03-09 14:33:13 +0200

    1.1.5.1b - Password is optional now in add user form if "Generate password if not present" is ticked in account plugin settings. If not provided, password will be auto-generated and emailed to account email address.

--- plugins.dist/admin/actions/phs_user_add.php
+++ plugins.dist/admin/actions/phs_user_add.php
@@ -3,7 +3,6 @@
 namespace phs\plugins\admin\actions;
 
 use \phs\PHS;
-use \phs\PHS_Bg_jobs;
 use \phs\PHS_Scope;
 use \phs\libraries\PHS_Action;
 use \phs\libraries\PHS_Params;
@@ -19,7 +18,7 @@ class PHS_Action_User_add extends PHS_Action
      */
     public function allowed_scopes()
     {
-        return array( PHS_Scope::SCOPE_WEB, PHS_Scope::SCOPE_AJAX );
+        return [PHS_Scope::SCOPE_WEB, PHS_Scope::SCOPE_AJAX];
     }
 
     /**
@@ -41,31 +40,16 @@ class PHS_Action_User_add extends PHS_Action
         }
 
         /** @var \phs\plugins\accounts\PHS_Plugin_Accounts $accounts_plugin */
-        if( !($accounts_plugin = PHS::load_plugin( 'accounts' ))
-         or !($accounts_plugin_settings = $accounts_plugin->get_plugin_settings()) )
-        {
-            PHS_Notifications::add_error_notice( $this->_pt( 'Couldn\'t load accounts plugin.' ) );
-            return self::default_action_result();
-        }
-
         /** @var \phs\plugins\accounts\models\PHS_Model_Accounts $accounts_model */
-        if( !($accounts_model = PHS::load_model( 'accounts', 'accounts' )) )
-        {
-            PHS_Notifications::add_error_notice( $this->_pt( 'Couldn\'t load accounts model.' ) );
-            return self::default_action_result();
-        }
-
         /** @var \phs\system\core\models\PHS_Model_Roles $roles_model */
-        if( !($roles_model = PHS::load_model( 'roles' )) )
-        {
-            PHS_Notifications::add_error_notice( $this->_pt( 'Couldn\'t load roles model.' ) );
-            return self::default_action_result();
-        }
-
         /** @var \phs\system\core\models\PHS_Model_Plugins $plugins_model */
-        if( !($plugins_model = PHS::load_model( 'plugins' )) )
+        if( !($accounts_plugin = PHS::load_plugin( 'accounts' ))
+         || !($accounts_plugin_settings = $accounts_plugin->get_plugin_settings())
+         || !($accounts_model = PHS::load_model( 'accounts', 'accounts' ))
+         || !($roles_model = PHS::load_model( 'roles' ))
+         || !($plugins_model = PHS::load_model( 'plugins' )) )
         {
-            PHS_Notifications::add_error_notice( $this->_pt( 'Couldn\'t load plugins model.' ) );
+            PHS_Notifications::add_error_notice( $this->_pt( 'Error loading required resources.' ) );
             return self::default_action_result();
         }
 
@@ -76,15 +60,15 @@ class PHS_Action_User_add extends PHS_Action
         }
 
         if( !($roles_by_slug = $roles_model->get_all_roles_by_slug()) )
-            $roles_by_slug = array();
+            $roles_by_slug = [];
 
         $foobar = PHS_Params::_p( 'foobar', PHS_Params::T_INT );
         $nick = PHS_Params::_p( 'nick', PHS_Params::T_NOHTML );
         $pass = PHS_Params::_p( 'pass', PHS_Params::T_ASIS );
         $email = PHS_Params::_p( 'email', PHS_Params::T_EMAIL );
         $level = PHS_Params::_p( 'level', PHS_Params::T_INT );
-        if( !($account_roles_slugs = PHS_Params::_p( 'account_roles_slugs', PHS_Params::T_ARRAY, array( 'type' => PHS_Params::T_NOHTML ) )) )
-            $account_roles_slugs = array();
+        if( !($account_roles_slugs = PHS_Params::_p( 'account_roles_slugs', PHS_Params::T_ARRAY, ['type' => PHS_Params::T_NOHTML])) )
+            $account_roles_slugs = [];
         $title = PHS_Params::_p( 'title', PHS_Params::T_NOHTML );
         $fname = PHS_Params::_p( 'fname', PHS_Params::T_NOHTML );
         $lname = PHS_Params::_p( 'lname', PHS_Params::T_NOHTML );
@@ -100,7 +84,7 @@ class PHS_Action_User_add extends PHS_Action
 
         if( !empty( $do_submit ) )
         {
-            $insert_arr = array();
+            $insert_arr = [];
             $insert_arr['nick'] = $nick;
             $insert_arr['pass'] = $pass;
             $insert_arr['email'] = $email;
@@ -108,14 +92,14 @@ class PHS_Action_User_add extends PHS_Action
             $insert_arr['status'] = $accounts_model::STATUS_ACTIVE;
             $insert_arr['added_by'] = $current_user['id'];
 
-            $insert_details_arr = array();
+            $insert_details_arr = [];
             $insert_details_arr['title'] = $title;
             $insert_details_arr['fname'] = $fname;
             $insert_details_arr['lname'] = $lname;
             $insert_details_arr['phone'] = $phone;
             $insert_details_arr['company'] = $company;
 
-            $insert_params_arr = array();
+            $insert_params_arr = [];
             $insert_params_arr['fields'] = $insert_arr;
             $insert_params_arr['{users_details}'] = $insert_details_arr;
             $insert_params_arr['{account_roles}'] = $account_roles_slugs;
@@ -127,19 +111,18 @@ class PHS_Action_User_add extends PHS_Action
 
                 $action_result = self::default_action_result();
 
-                $action_result['redirect_to_url'] = PHS::url( array( 'p' => 'admin', 'a' => 'users_list' ), array( 'account_created' => 1 ) );
+                $action_result['redirect_to_url'] = PHS::url( ['p' => 'admin', 'a' => 'users_list'], ['account_created' => 1]);
 
                 return $action_result;
-            } else
-            {
-                if( $accounts_model->has_error() )
-                    PHS_Notifications::add_error_notice( $accounts_model->get_error_message() );
-                else
-                    PHS_Notifications::add_error_notice( $this->_pt( 'Error saving details to database. Please try again.' ) );
             }
+
+            if( $accounts_model->has_error() )
+                PHS_Notifications::add_error_notice( $accounts_model->get_error_message() );
+            else
+                PHS_Notifications::add_error_notice( $this->_pt( 'Error saving details to database. Please try again.' ) );
         }
 
-        $data = array(
+        $data = [
             'nick' => $nick,
             'pass' => $pass,
             'level' => $level,
@@ -159,7 +142,7 @@ class PHS_Action_User_add extends PHS_Action
 
             'roles_model' => $roles_model,
             'plugins_model' => $plugins_model,
-        );
+        ];
 
         return $this->quick_render_template( 'user_add', $data );
     }
