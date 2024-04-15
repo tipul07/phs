991ef3a774c81c33caa2f340494f81c95df1d74b
Andrei Orghici <aorghici@gmail.com>
2024-04-15 21:46:52 +0300

    1.2.0.12c - Fixing strong type in controller classes

--- system/libraries/phs_controller_admin.php
+++ system/libraries/phs_controller_admin.php
@@ -13,7 +13,7 @@ abstract class PHS_Controller_Admin extends PHS_Controller
         return true;
     }
 
-    public function allowed_scopes() : array
+    public function allowed_scopes()
     {
         return [PHS_Scope::SCOPE_WEB, PHS_Scope::SCOPE_AJAX];
     }
